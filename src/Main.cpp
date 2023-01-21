1a66974121dbe9d390c3a927583037895559a94c
12jikan <jareem.e.hoff@gmail.com>
2023-01-21 15:48:16 -0500

    added glm library

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,12 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <math.h>
-#include "headers/Shader.h"
 #include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "headers/Shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
