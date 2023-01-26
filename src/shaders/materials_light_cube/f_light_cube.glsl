99e916a37a5e4fded6186de1bd6a6411b3ee2e12
Jareem Hoff <jareem.e.hoff@gmail.com>
2023-01-26 18:09:18 -0500

    multiple green lights

--- src/shaders/materials_light_cube/f_light_cube.glsl
+++ src/shaders/materials_light_cube/f_light_cube.glsl
@@ -3,5 +3,5 @@ out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(1.0); // set all 4 vector values to 1.0
+    FragColor = vec4(0.0f, 1.00f, 0.00f, 1.00f); // set all 4 vector values to 1.0
 }
